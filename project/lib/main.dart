ad61916f6fc024c9560af50fd966c5c1b7a22dfd
LYNGOCBACH <bach121103@gmail.com>
2024-01-04 21:52:44 +0700

    Update logout

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -1,60 +1,60 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:todoflutter/screens/login_screen.dart';
-import 'package:todoflutter/screens/signup_screen.dart';
-import 'package:todoflutter/screens/welcome.dart';
-import 'pages/home_page.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+  import 'package:firebase_auth/firebase_auth.dart';
+  import 'package:flutter/material.dart';
+  import 'package:hive_flutter/hive_flutter.dart';
+  import 'package:todoflutter/screens/login_screen.dart';
+  import 'package:todoflutter/screens/signup_screen.dart';
+  import 'package:todoflutter/screens/welcome.dart';
+  import 'pages/home_page.dart';
+  import 'package:firebase_core/firebase_core.dart';
+  import 'firebase_options.dart';
+  import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  // init the hive
-  await Hive.initFlutter();
-
-  // open a box
-  var box = await Hive.openBox('mybox');
-
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const Welcome(),
-      initialRoute: 'welcome',
-routes: {
-  '/login': (context) => LoginScreen(),
-  '/signup': (context) => SignUpScreen(),
-  // '/homepage': ((context) => HomePage()),
-  '/welcome': (context) => Welcome(),
-  // '/home': (context) => Home(),
-},
-      theme: ThemeData(primarySwatch: Colors.yellow),
+  void main() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
     );
-  }
-}
-// class Home extends StatelessWidget {
-//   const Home({Key? key}) : super(key: key);
+    // init the hive
+    await Hive.initFlutter();
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Home'),
-//       ),
-//       body: Center(
-//         child: Text('Welcome to the Home screen!'),
-//       ),
-//     );
-//   }
-// }
+    // open a box
+    var box = await Hive.openBox('mybox');
+
+    runApp(const MyApp());
+  }
+
+  class MyApp extends StatelessWidget {
+    const MyApp({Key? key}) : super(key: key);
+
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: const Welcome(),
+        initialRoute: 'welcome',
+  routes: {
+    '/login': (context) => LoginScreen(),
+    '/signup': (context) => SignUpScreen(),
+    // '/homepage': ((context) => HomePage()),
+    '/welcome': (context) => Welcome(),
+    // '/home': (context) => Home(),
+  },
+        theme: ThemeData(primarySwatch: Colors.yellow),
+      );
+    }
+  }
+  // class Home extends StatelessWidget {
+  //   const Home({Key? key}) : super(key: key);
+
+  //   @override
+  //   Widget build(BuildContext context) {
+  //     return Scaffold(
+  //       appBar: AppBar(
+  //         title: Text('Home'),
+  //       ),
+  //       body: Center(
+  //         child: Text('Welcome to the Home screen!'),
+  //       ),
+  //     );
+  //   }
+  // }
--- project/lib/pages/home_page.dart
+++ project/lib/pages/home_page.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:todoflutter/screens/welcome.dart';
 import 'package:todoflutter/util/todo_database_service.dart';
 import '../data/database.dart';
 import '../util/dialog_box.dart';
@@ -17,42 +19,45 @@ class _HomePageState extends State<HomePage> {
   final _myBox = Hive.box('mybox');
   final ToDoDataBase db = ToDoDataBase();
   final ToDoDatabaseService db2 = ToDoDatabaseService();
+  late final FirebaseAuth _firebaseAuth;
 
   @override
   void initState() {
 
     if (_myBox.get("TODOLIST") == null) {
+
       db.createInitialData();
     } else {
-      
+
       db.loadData();
     }
 
     super.initState();
+    _firebaseAuth = FirebaseAuth.instance;
   }
 
   // Text controller
   final _controller = TextEditingController();
 
-  // Checkbox 
+  // Checkbox
   void checkBoxChanged(bool value, int index) {
     // Update Cloud Firestore
     db2.updateTask(db.toDoList[index].id, value);
   }
 
   // Save new task
-void saveNewTask() async {
-  await db2.createNewTodo(_controller.text);
-  _controller.clear();
-  Navigator.of(context).pop();
+  void saveNewTask() async {
+    await db2.createNewTodo(_controller.text);
+    _controller.clear();
+    Navigator.of(context).pop();
 
-  // setstate Cloud Firestore
-  await db2.listTodo().listen((List<ToDoTask> data) {
-    setState(() {
-      db.toDoList = data;
+    // setstate Cloud Firestore
+    await db2.listTodo().listen((List<ToDoTask> data) {
+      setState(() {
+        db.toDoList = data;
+      });
     });
-  });
-}
+  }
 
 
   // Create a new task
@@ -84,17 +89,68 @@ void saveNewTask() async {
         leading: IconButton(
           icon: Icon(Icons.menu),
           onPressed: () {
-            
+
             Scaffold.of(context).openDrawer();
           },
         ),
+        actions: [
+          PopupMenuButton(
+            icon: CircleAvatar(
+              child: Image.network(
+                'https://scontent.fhan5-10.fna.fbcdn.net/v/t39.30808-6/323004993_1144226476263200_2305308931759181719_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=0MU7PWj222EAX9Y7GXY&_nc_ht=scontent.fhan5-10.fna&oh=00_AfAasNtvi-mD2rbnYJVqaXAJ7DK7o4Dn_BuJPhB5gA6vew&oe=659C9670',
+                width: 100,
+                height: 100,
+              ),
+            ),
+            itemBuilder: (BuildContext context) {
+              return [
+                PopupMenuItem(
+                  child: ListTile(
+                    leading: Icon(Icons.exit_to_app),
+                    title: Text('Logout'),
+                    onTap: () {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text('Xác nhận Logout'),
+                            content: Text('Bạn có chắc chắn muốn log out?'),
+                            actions: [
+                              TextButton(
+                                onPressed: () async {
+                                  await _firebaseAuth.signOut();
+                                  Navigator.pushNamedAndRemoveUntil(
+                                    context,
+                                    '/welcome', // You may change this to the initial route of your app
+                                    (Route<dynamic> route) => false,
+                                  );
+                                },
+                                child: Text('Yes'),
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text('No'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                  ),
+                ),
+              ];
+            },
+          ),
+        ],
         elevation: 0,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: createNewTask,
         child: Icon(Icons.add),
       ),
-            body: StreamBuilder<List<ToDoTask>>(
+      body: StreamBuilder<List<ToDoTask>>(
         stream: db2.listTodo(),
         builder: (context, snapshot) {
           if (snapshot.hasError) {
@@ -118,8 +174,10 @@ void saveNewTask() async {
                 taskName: todoList[index].taskName,
                 taskCompleted: todoList[index].taskCompleted,
                 id: todoList[index].id,
-                onChanged: (value) => checkBoxChanged(value!, todoList[index].id as int),
-                deleteFunction: (context) => deleteTask(todoList[index].id as int),
+                onChanged: (value) =>
+                    checkBoxChanged(value!, todoList[index].id as int),
+                deleteFunction: (context) =>
+                    deleteTask(todoList[index].id as int),
               );
             },
           );
